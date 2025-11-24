a6320058f777459d101824765b241cce286abeda
prjsghq7 <prjsghq7@gmail.com>
2025-11-24 21:33:38 +0900

    Init: Spring Boot backend project setup

---
+++ src/main/java/com/dev/workhiveback/WorkHiveBackApplication.java
@@ -0,0 +1,13 @@
+package com.dev.workhiveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHiveBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHiveBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/workhiveback/WorkHiveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.workhiveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHiveBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
