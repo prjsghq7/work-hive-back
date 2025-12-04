fc567c148274db2ceff8aa975cd901c236e2bb6b
prjsghq7 <prjsghq7@gmail.com>
2025-12-04 18:10:15 +0900

    [김건호, 초기 프로젝트 구조 설정] json, Spring Security 의존성 추가 Conrtoller 추가 및 Test 추가

---
+++ src/main/java/com/dev/workhiveback/controllers/TestController.java
@@ -0,0 +1,32 @@
+package com.dev.workhiveback.controllers;
+
+import com.dev.workhiveback.entities.UserEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @RequestMapping(value = "/requestTest",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> requestTest() throws JsonProcessingException {
+
+        JSONObject response = new JSONObject();
+        response.put("result", "requestTest");
+
+        UserEntity user = new UserEntity();
+        user.setName("test");
+
+
+        response.put("user", user);
+
+        HttpStatus status = HttpStatus.OK;
+        return ResponseEntity.status(status).body(response);
+    }
+}
